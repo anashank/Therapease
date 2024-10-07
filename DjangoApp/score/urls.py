759ec2b60e1d108ca904f6341823ef547480ae47
TogeSalmon <yidampark@gmail.com>
2024-10-07 15:09:24 -0700

    commenting out logout.html

--- DjangoApp/score/urls.py
+++ DjangoApp/score/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('save-type/', views.save_type, name='save_type'),  # Save user type (Therapist/User)
     path('register/', views.register, name='register'),  # User registration view
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),  # Login view
-    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),  # Logout view
+    #path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),  # Logout view
     path('run-python/', views.run_python_code, name='run_python_code'),  # Endpoint to run Python code
     path('get-recent-match/', views.get_recent_match, name='get_recent_match'),  # Fetch the most recent match
 ]
